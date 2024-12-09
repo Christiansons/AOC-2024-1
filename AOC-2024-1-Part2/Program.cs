597cbb594bb7028473a64a0534e584d55ff23bd3
Alex <alexander.christianson@outlook.com>
2024-12-09 14:35:20 +0100

    aoc day 1

---
+++ AOC-2024-1-Part2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AOC_2024_1_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var path = "C:\\Users\\Alex\\source\\repos\\AOC-2024-1\\input.txt";
+            string[] allLines = File.ReadAllLines(path);
+
+            int[] leftColumn = allLines.Select(line => Convert.ToInt32(line.Split(" ").First())).ToArray();
+            int[] rightColumn = allLines.Select(line => Convert.ToInt32(line.Split(" ").Last())).ToArray();
+
+            int sum = 0;
+
+            for (int i = 0; i < allLines.Length; i++) {
+                int instances = rightColumn.Where(l => l == leftColumn[i]).Count();
+                sum += instances * leftColumn[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
